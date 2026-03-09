6d5d1cac9e06927079c9b3c15afd4d425cfb7f30
Aaron <93011622+SenergyGroup@users.noreply.github.com>
2026-03-08 21:42:39 -0500

    Updated language to just download files

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ const renderConfirmedPage = (claim, widget) =>
         </div>
 
         <div class="button-group">
-          <a class="primary-button" href="/download/${claim.claim_token}">Download ZIP File</a>
+          <a class="primary-button" href="/download/${claim.claim_token}">Download File</a>
           <!-- ADDED YOUR ETSY LINK HERE -->
           <a class="secondary-button" href="https://www.etsy.com/shop/SenergyGroup?ref=dashboard-header" target="_blank">Browse Our Other Themes on Etsy</a>
         </div>
